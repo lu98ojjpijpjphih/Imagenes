72a56347041a3a156930358084e6898a73eedea7
LuisanaGarello <164653068+lu98ojjpijpjphih@users.noreply.github.com>
2024-10-10 08:39:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let p = document.querySelector("p")
+let boton1 = document.querySelector("#boton1")
+let boton2 = document.querySelector("#boton2")
+let boton3 = document.querySelector("#boton3")
+let boton4 = document.querySelector("#boton4")
+let imagen1 = document.querySelector("#imagen1")
+
+boton1.onclick = function(){
+imagen1.src = 'https://img.remediosdigitales.com/69b18a/porsche-911-turbo-dkr/450_1000.jpg'
+}
+
+boton2.onclick = function(){
+imagen1.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Porsche_918_Spyder_IAA_2013.jpg/1200px-Porsche_918_Spyder_IAA_2013.jpg'
+}
+
+boton3.onclick = function(){
+imagen1.src = 'https://images-porsche.imgix.net/-/media/0CBE07BEB4F84337B9F39F2EBAF4F771_37A40E67D5904D95958C5915FB4269B5_TA24Q3EIX0002-taycan-turbo-s-front?w=999&q=85&auto=format'
+}
+
+boton4.onclick = function(){
+imagen1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqBAZbv1-XCDQzSXs0VDT6ctS23f76WoftMA&s'
+}
